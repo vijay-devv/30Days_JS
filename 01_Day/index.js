e6a2fd99d11a98d8331b4ff2a98217b491c4e136
vijaydev <vijaydudhal2019@gmail.com>
2024-07-13 18:59:33 +0530

    updated file..

--- 01_Day/index.js
+++ 01_Day/index.js
@@ -1,37 +1,37 @@
-// // task 1
-// var a=30;
-// let b="vijay";
-// const c=true;
+// task 1
+var a=30;
+let b="vijay";
+const c=true;
 
 
 // // Tassk 2
-// console.log(a);
-// console.log(b);
-// console.log(c);
+console.log(a);
+console.log(b);
+console.log(c);
 
 
 // task 3
-// let aaa=10
-// let name="vijay!"
-// let h=true;
-// let gg={name:"vijay!",
-//     age :24
-// }
-// const myarray=["vijay", "vijay", "vijay"];
+let aaa=10
+let name="vijay!"
+let h=true;
+let gg={name:"vijay!",
+    age :24
+}
+const myarray=["vijay", "vijay", "vijay"];
 
-// console.log(typeof(aaa));
-// console.log(typeof(name));
-// console.log(typeof(h));
-// console.log(typeof(gg));
-// console.log(typeof myarray);
+console.log(typeof(aaa));
+console.log(typeof(name));
+console.log(typeof(h));
+console.log(typeof(gg));
+console.log(typeof myarray);
 
 
 // task4
 
-// let a=10;
-// console.log(a);
-// a=40;
-// console.log(a);
+let a=10;
+console.log(a);
+a=40;
+console.log(a);
 
 // task 5
 
