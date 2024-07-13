86e5fd0d29f90d8bfff8e737bb60fc46ab6b7463
vijaydev <vijaydudhal2019@gmail.com>
2024-07-13 18:56:33 +0530

    day 1 activity completed.............

---
+++ 01_Day/index.js
@@ -0,0 +1,44 @@
+// // task 1
+// var a=30;
+// let b="vijay";
+// const c=true;
+
+
+// // Tassk 2
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// task 3
+// let aaa=10
+// let name="vijay!"
+// let h=true;
+// let gg={name:"vijay!",
+//     age :24
+// }
+// const myarray=["vijay", "vijay", "vijay"];
+
+// console.log(typeof(aaa));
+// console.log(typeof(name));
+// console.log(typeof(h));
+// console.log(typeof(gg));
+// console.log(typeof myarray);
+
+
+// task4
+
+// let a=10;
+// console.log(a);
+// a=40;
+// console.log(a);
+
+// task 5
+
+
+const a=10;
+console.log(a);
+a=40;
+console.log(a);
+
+// error =TypeError: Assignment to constant variable.
