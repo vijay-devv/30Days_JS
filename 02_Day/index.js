8c64dfd62486c2ac77c601d27241d3d05ddd0b77
vijaydev <vijaydudhal2019@gmail.com>
2024-07-14 16:02:16 +0530

    this day02 leaning various types operator

---
+++ 02_Day/index.js
@@ -0,0 +1,95 @@
+// task 1
+// const a=10
+// const b=20
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// task2
+
+// var  num=5
+
+// num+=3
+// num-=3
+
+// console.log(num);
+
+// task 3
+
+// let num1 = 10;
+// let num2 = 5;
+
+
+// if (num1 > num2) {
+//     console.log(`${num1} is greater than ${num2}`);
+// } else {
+//     console.log(`${num1} is not greater than ${num2}`);
+// }
+
+
+// if (num1 < num2) {
+//     console.log(`${num1} is less than ${num2}`);
+// } else {
+//     console.log(`${num1} is not less than ${num2}`);
+// }
+
+
+
+
+// let num1 = 10;
+// let num2 = 5;
+
+
+// if (num1 >= num2) {
+//     console.log(`${num1} is greater than or equal to ${num2}`);
+// } else {
+//     console.log(`${num1} is not greater than or equal to ${num2}`);
+// }
+
+
+// if (num1 <= num2) {
+//     console.log(`${num1} is less than or equal to ${num2}`);
+// } else {
+//     console.log(`${num1} is not less than or equal to ${num2}`);
+// }
+
+
+// let age = 25;
+// let hasDriverLicense = true;
+
+
+// if (age >= 18 && hasDriverLicense) {
+//     console.log("You are eligible to drive.");
+// } else {
+//     console.log("You are not eligible to drive.");
+// }
+
+
+
+// let student = true;
+// let emp = false;
+
+
+// if (student || emp) {
+//     console.log("You are either a student or an employee.");
+// } else {
+//     console.log("You are neither a student not an employee.");
+// }
+
+
+// let isLoggedOut = false;
+// if (!isLoggedOut) {
+//     console.log("User is logged in.");
+// } else {
+//     console.log("User is not logged in.");
+// }
+
+
+let num=10;
+
+const isprime =(num/2)?"no is primme":"no is not prime";
+
+console.log(isprime);
